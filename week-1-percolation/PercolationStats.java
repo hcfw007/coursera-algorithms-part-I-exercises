e3c3be73f696239fac23eef7a516f5831022fd7c
hcfw007 <brotherstyx@gmail.com>
2020-06-09 16:57:46 +0800

    一个看起来可用的PercolationStats实现

--- week-1-percolation/PercolationStats.java
+++ week-1-percolation/PercolationStats.java
@@ -1,21 +1,63 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+    private double[] results;
+    private int T;
+    private int n;
 
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials)
+    public PercolationStats(int n, int trials) {
+        if (n <=0 || trials <= 0) {
+            throw new IllegalArgumentException("n and trials must be greater than 0!");
+        }
+        this.n = n;
+        this.T = trials;
+        this.results = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Percolation percolation = new Percolation(n);
+            int count = 0;
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                while (percolation.isOpen(row, col)) {
+                    row = StdRandom.uniform(1, n + 1);
+                    col = StdRandom.uniform(1, n + 1);
+                }
+                percolation.open(row, col);
+                count ++;
+            }
+            this.results[i] = ((float)count) / (n * n);
+        }
+    }
 
     // sample mean of percolation threshold
-    public double mean()
+    public double mean() {
+        return StdStats.mean(this.results);
+    }
 
     // sample standard deviation of percolation threshold
-    public double stddev()
+    public double stddev() {
+        return StdStats.stddev(this.results);
+    }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo()
+    public double confidenceLo() {
+        return this.mean() - Math.sqrt(this.stddev()) * 1.96 / Math.sqrt(this.T);
+    }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi()
+    public double confidenceHi() {
+        return this.mean() + Math.sqrt(this.stddev()) * 1.96 / Math.sqrt(this.T);
+    }
 
-   // test client (see below)
-   public static void main(String[] args)
-
+    // test client (see below)
+    public static void main(String[] args) {
+        int n = 200;
+        int t = 100;
+        PercolationStats ps = new PercolationStats(n, t);
+        System.out.println("mean = " + ps.mean());
+        System.out.println("stddev = " + ps.stddev());
+        System.out.println("95% confidence intercal = [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
+    }
 }
